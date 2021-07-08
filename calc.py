ceeb8882a93b372c408d372b7212f5f33aa7e627
mgueye11 <maimounag096@gmail.com>
2021-07-08 14:36:44 +0000

    adding while loop

---
+++ calc.py
@@ -0,0 +1,15 @@
+# Greet the user
+print("Hello, I'm a fortun teller")
+
+# Ask the user if they want their fortune told
+answer = input("Do you want a fortune? ")
+
+while answer.lower() != "no":
+    print("User asked for a fortune")
+    answer = input("Do you want a fortune? ")
+
+# Fortune given
+
+# Ask if user wants another fortune 
+
+# Repeat until user quits
